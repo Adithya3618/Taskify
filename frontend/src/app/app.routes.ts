fb2854289dc2b6a1f5aea61808a8820da5d47d12
meghana21-arch <saimeghana.barla@gmail.com>
2026-02-17 14:02:23 -0500

    feat(frontend): add login and signup routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+  { path: 'signup', loadComponent: () => import('./pages/signup/signup.component').then(m => m.SignupComponent) },
   { path: 'board/:id', loadComponent: () => import('./pages/board/board.component').then(m => m.BoardComponent) },
   { path: '**', redirectTo: '' }
 ];
