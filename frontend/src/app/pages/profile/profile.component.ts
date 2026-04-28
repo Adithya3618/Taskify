f8e1c10b09686857e8f62ebb915ac58fdd58ca69
saisreejachava <saisreejchava@ufl.edu>
2026-04-28 14:14:24 -0400

    add profile route shell

---
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent {}
